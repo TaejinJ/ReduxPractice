526e5870b333978b733f731cfe2286686c06df77
taejin jo <xowls296@gmail.com>
2023-07-03 22:53:54 +0900

    combineReducers

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 
 type Props = {
-  value: number;
+  value: any;
   onIncrement: () => void;
   onDecrement: () => void;
 };
 export default function App({ value, onIncrement, onDecrement }: Props) {
   return (
     <p>
-      counter: {value} time <button onClick={onIncrement}>+</button>
+      {/* counter: {value} time  */}
+      <button onClick={onIncrement}>+</button>
       <button onClick={onDecrement}>-</button>
     </p>
   );
